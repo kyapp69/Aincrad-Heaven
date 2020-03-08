79bc3b4de7fec0b0d0ecc7b3005c412ec82d54d4
KazuGod <kazuzmafii@gmail.com>
2020-03-08 01:32:35 +0100

    Scripted buttons, background music, info and levels sections

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +21,21 @@ public class MenuController : MonoBehaviour
     public void Play()
     {
         SceneManager.LoadScene("Start", LoadSceneMode.Single);
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+    public void Info()
+    {
+        SceneManager.LoadScene("Info", LoadSceneMode.Single);
+    }
+    public void Levels()
+    {
+        SceneManager.LoadScene("Levels", LoadSceneMode.Single);
+    }
+    public void LoadedMenu()
+    {
+        SceneManager.LoadScene("LoadedMenu", LoadSceneMode.Single);
     }
 }
---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AudioSource m_MyAudioSource;
+    bool m_Play;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_Play)
+        {
+            m_MyAudioSource.Play();
+        }
+    }
+}
